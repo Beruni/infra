dc405797a041d0db65b9732b87fe188f20c7fbcd
Rohit Garg <rohitgarg19@gmail.com>
2016-09-12 12:40:33 +0530

    +Parmatma | Added extra message for pressing enter to continue

--- base_setup_gitlab_ci_runner.sh
+++ base_setup_gitlab_ci_runner.sh
@@ -5,6 +5,9 @@ brew tap caskroom/cask
 brew install docker
 brew install docker-machine
 brew install heroku-toolbelt
+echo "*********Going to login via SSO"
+echo "\033[0;31m*********Make sure you press an extra <enter> after the logged in message for script to continue...\033[0m"
+
 bash -c '(echo -e "thoughtworks-labs" && cat) | heroku login --sso'
 docker-machine start default
 ansible-playbook gitlab_ci_agent.yml -i inventory
